cf3521f3623b4cf349c42e67ab27c044b25afba4
PC <denis.kosogov@gmail.com>
2022-09-13 22:16:32 -0400

    fixed image objects

--- public/images.js
+++ public/images.js
@@ -3,143 +3,140 @@ export const images = [
         id: '1',
         title: 'Signage, POP',
         image: require('../assets/signage_pop/pop_00.jpg'),
-        collection: [{
-            src: require('../assets/signage_pop/pop_01.jpg'), width: '1467', height: '2665',
-            src: require('../assets/signage_pop/pop_02.jpg'), width: '900', height: '1400',
-            src: require('../assets/signage_pop/pop_03.jpg'), width: '2570', height: '320',
-            src: require('../assets/signage_pop/pop_04.jpg'), width: '2570', height: '540',
-            src: require('../assets/signage_pop/pop_05.jpg'), width: '2570', height: '340',
-            src: require('../assets/signage_pop/pop_06.jpg'), width: '2570', height: '460',
-            src: require('../assets/signage_pop/pop_07.jpg'), width: '1000', height: '2455',
-            src: require('../assets/signage_pop/pop_08.jpg'), width: '1000', height: '2455',
-            src: require('../assets/signage_pop/pop_09.jpg'), width: '2655', height: '720',
-            src: require('../assets/signage_pop/pop_10.jpg'), width: '2592', height: '1440',
-        }]
+        collection: [
+            { src: require('../assets/signage_pop/pop_01.jpg'), width: '1467', height: '2665' },
+            { src: require('../assets/signage_pop/pop_02.jpg'), width: '900', height: '1400' },
+            { src: require('../assets/signage_pop/pop_03.jpg'), width: '2570', height: '320' },
+            { src: require('../assets/signage_pop/pop_04.jpg'), width: '2570', height: '540' },
+            { src: require('../assets/signage_pop/pop_05.jpg'), width: '2570', height: '340' },
+            { src: require('../assets/signage_pop/pop_06.jpg'), width: '2570', height: '460' },
+            { src: require('../assets/signage_pop/pop_07.jpg'), width: '1000', height: '2455' },
+            { src: require('../assets/signage_pop/pop_08.jpg'), width: '1000', height: '2455' },
+            { src: require('../assets/signage_pop/pop_09.jpg'), width: '2655', height: '720' },
+            { src: require('../assets/signage_pop/pop_10.jpg'), width: '2592', height: '1440' }
+        ]
     },
     {
         id: '2',
         title: 'Flyers, Sell Sheets',
         image: require('../assets/flyers_sell_sheets/ss_00.jpg'),
-        collection: [{
-            src: require('../assets/flyers_sell_sheets/ss_01.jpg'), width: '2560', height: '2320',
-            src: require('../assets/flyers_sell_sheets/ss_02.jpg'), width: '2560', height: '2320',
-            src: require('../assets/flyers_sell_sheets/ss_03.jpg'), width: '2560', height: '2320',
-            src: require('../assets/flyers_sell_sheets/ss_04.jpg'), width: '2560', height: '2320',
-            src: require('../assets/flyers_sell_sheets/flyer_01.jpg'), width: '2500', height: '2400',
-            src: require('../assets/flyers_sell_sheets/flyer_02.jpg'), width: '2500', height: '2400',
-            src: require('../assets/flyers_sell_sheets/flyer_03.jpg'), width: '2500', height: '2400',
-            src: require('../assets/flyers_sell_sheets/flyer_04.jpg'), width: '2500', height: '2400',
-            src: require('../assets/flyers_sell_sheets/flyer_05.jpg'), width: '2500', height: '2400',
-        }]
+        collection: [
+            { src: require('../assets/flyers_sell_sheets/ss_01.jpg'), width: '2560', height: '2320' },
+            { src: require('../assets/flyers_sell_sheets/ss_02.jpg'), width: '2560', height: '2320' },
+            { src: require('../assets/flyers_sell_sheets/ss_03.jpg'), width: '2560', height: '2320' },
+            { src: require('../assets/flyers_sell_sheets/ss_04.jpg'), width: '2560', height: '2320' },
+            { src: require('../assets/flyers_sell_sheets/flyer_01.jpg'), width: '2500', height: '2400' },
+            { src: require('../assets/flyers_sell_sheets/flyer_02.jpg'), width: '2500', height: '2400' },
+            { src: require('../assets/flyers_sell_sheets/flyer_03.jpg'), width: '2500', height: '2400' },
+            { src: require('../assets/flyers_sell_sheets/flyer_04.jpg'), width: '2500', height: '2400' },
+            { src: require('../assets/flyers_sell_sheets/flyer_05.jpg'), width: '2500', height: '2400' }
+        ]
     },
     {
         id: '3',
         title: 'Labeling, Packaging',
         image: require('../assets/labeling/labels_00.jpg'),
-        collection: [{
-            src: require('../assets/labeling/labels_01.jpg'), width: '3300', height: '2550',
-            src: require('../assets/labeling/labels_02.jpg'), width: '3300', height: '2400',
-            src: require('../assets/labeling/labels_03.jpg'), width: '3300', height: '1231',
-        }]
+        collection: [
+            { src: require('../assets/labeling/labels_01.jpg'), width: '3300', height: '2550' },
+            { src: require('../assets/labeling/labels_02.jpg'), width: '3300', height: '2400' },
+            { src: require('../assets/labeling/labels_03.jpg'), width: '3300', height: '1231' },
+        ]
     },
     {
         id: '4',
         title: 'Books, Catalogues',
         image: require('../assets/books_catalogues/book_00.jpg'),
-        collection: [{
-            src: require('../assets/books_catalogues/book_01.jpg'), width: '3300', height: '2550',
-            src: require('../assets/books_catalogues/book_02.jpg'), width: '3300', height: '2550',
-            src: require('../assets/books_catalogues/book_03.jpg'), width: '3300', height: '2550',
-            src: require('../assets/books_catalogues/catalogues_01.jpg'), width: '3000', height: '2000',
-            src: require('../assets/books_catalogues/catalogues_02.jpg'), width: '4500', height: '3000',
-            src: require('../assets/books_catalogues/catalogues_03.jpg'), width: '3300', height: '2550',
-            src: require('../assets/books_catalogues/manual_01.jpg'), width: '3065', height: '2038',
-        }]
+        collection: [
+            { src: require('../assets/books_catalogues/book_01.jpg'), width: '3300', height: '2550' },
+            { src: require('../assets/books_catalogues/book_02.jpg'), width: '3300', height: '2550' },
+            { src: require('../assets/books_catalogues/book_03.jpg'), width: '3300', height: '2550' },
+            { src: require('../assets/books_catalogues/catalogues_01.jpg'), width: '3000', height: '2000' },
+            { src: require('../assets/books_catalogues/catalogues_02.jpg'), width: '4500', height: '3000' },
+            { src: require('../assets/books_catalogues/catalogues_03.jpg'), width: '3300', height: '2550' },
+            { src: require('../assets/books_catalogues/manual_01.jpg'), width: '3065', height: '2038' },
+        ]
     },
     {
         id: '5',
         title: '3D Modeling',
         image: require('../assets/3d/3d_00.jpg'),
-        collection: [{
-            src: require('../assets/3d/3d_01.jpg'), width: '3000', height: '1359',
-            src: require('../assets/3d/3d_02.jpg'), width: '3000', height: '819',
-            src: require('../assets/3d/3d_03.jpg'), width: '3000', height: '819',
-            src: require('../assets/3d/3d_04.jpg'), width: '1500', height: '1223',
-            src: require('../assets/3d/3d_05.jpg'), width: '1500', height: '1245',
-            src: require('../assets/3d/3d_06.jpg'), width: '1500', height: '1089',
-            src: require('../assets/3d/3d_07.jpg'), width: '3000', height: '1325',
-            src: require('../assets/3d/3d_08.jpg'), width: '3000', height: '1325',
-            src: require('../assets/3d/3d_09.jpg'), width: '3000', height: '889',
-            src: require('../assets/3d/3d_10.jpg'), width: '1500', height: '1217',
-            src: require('../assets/3d/3d_11.jpg'), width: '1500', height: '1027',
-            src: require('../assets/3d/3d_12.jpg'), width: '1500', height: '1500',
-            src: require('../assets/3d/3d_13.jpg'), width: '3000', height: '819',
-
-        }]
+        collection: [
+            { src: require('../assets/3d/3d_01.jpg'), width: '3000', height: '1359' },
+            { src: require('../assets/3d/3d_02.jpg'), width: '3000', height: '819' },
+            { src: require('../assets/3d/3d_03.jpg'), width: '3000', height: '819' },
+            { src: require('../assets/3d/3d_04.png'), width: '1500', height: '1223' },
+            { src: require('../assets/3d/3d_05.jpg'), width: '1500', height: '1245' },
+            { src: require('../assets/3d/3d_06.jpg'), width: '1500', height: '1089' },
+            { src: require('../assets/3d/3d_07.jpg'), width: '3000', height: '1325' },
+            { src: require('../assets/3d/3d_08.jpg'), width: '3000', height: '1325' },
+            { src: require('../assets/3d/3d_09.jpg'), width: '3000', height: '889' },
+            { src: require('../assets/3d/3d_10.jpg'), width: '1500', height: '1217' },
+            { src: require('../assets/3d/3d_11.jpg'), width: '1500', height: '1027' },
+            { src: require('../assets/3d/3d_12.jpg'), width: '1500', height: '1500' },
+            { src: require('../assets/3d/3d_13.jpg'), width: '3000', height: '819' }
+        ]
     },
     {
         id: '6',
         title: 'Product Photography',
         image: require('../assets/product_photo/photo_00.jpg'),
-        collection: [{
-            src: require('../assets/product_photo/photo_01.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_02.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_03.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_04.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_05.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_06.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_07.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_08.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_09.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_10.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_11.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_12.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_13.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_14.jpg'), width: '2000', height: '2000',
-            src: require('../assets/product_photo/photo_15.jpg'), width: '2000', height: '2000',
-
-        }]
+        collection: [
+            { src: require('../assets/product_photo/photo_01.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_02.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_03.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_04.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_05.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_06.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_07.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_08.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_09.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_10.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_11.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_12.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_13.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_14.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/product_photo/photo_15.jpg'), width: '2000', height: '2000' },
+        ]
     },
     {
         id: '7',
         title: 'Stock Photography',
         image: require('../assets/stock_photo/stock_00.jpg'),
-        collection: [{
-            src: require('../assets/stock_photo/stock_11.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_12.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_13.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_21.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_22.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_23.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_31.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_32.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_33.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_41.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_42.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_43.jpg'), width: '2800', height: '1800',
-            src: require('../assets/stock_photo/stock_61.jpg'), width: '2000', height: '2000',
-           
-        }]
+        collection: [
+            { src: require('../assets/stock_photo/stock_11.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_12.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_13.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_21.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_22.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_23.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_31.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_32.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_33.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_41.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_42.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_43.jpg'), width: '2800', height: '1800' },
+            { src: require('../assets/stock_photo/stock_61.jpg'), width: '2000', height: '2000' },
+        ]
     },
     {
         id: '8',
         title: 'Business Stationary',
         image: require('../assets/stationery/stationery_00.jpg'),
-        collection: [{
-            src: require('../assets/stationery/stationery_01.jpg'), width: '3000', height: '1980',
-        }]
+        collection: [
+            { src: require('../assets/stationery/stationery_01.jpg'), width: '3000', height: '1980' },
+        ]
     },
     {
         id: '9',
         title: 'Line Drawing',
         image: require('../assets/line/line_00.jpg'),
-        collection: [{
-            src: require('../assets/line/line_01.jpg'), width: '2000', height: '2000',
-            src: require('../assets/line/line_02.jpg'), width: '2000', height: '2000',
-            src: require('../assets/line/line_03.jpg'), width: '2000', height: '2000',
-            src: require('../assets/line/line_04.jpg'), width: '2000', height: '2000',
-            src: require('../assets/line/line_05.jpg'), width: '2000', height: '2000',
-            src: require('../assets/line/line_06.jpg'), width: '2000', height: '2000',
-        }]
+        collection: [
+            { src: require('../assets/line/line_01.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/line/line_02.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/line/line_03.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/line/line_04.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/line/line_05.jpg'), width: '2000', height: '2000' },
+            { src: require('../assets/line/line_06.jpg'), width: '2000', height: '2000' },
+        ]
     }
 ]
