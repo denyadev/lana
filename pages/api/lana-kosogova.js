2003bb818a7771c9c14b26b2373167d8d1c50820
PC <denis.kosogov@gmail.com>
2022-10-28 21:56:49 -0400

    Changed resume, removed local images, added lighthouse, allowed cloudinary

--- pages/api/lana-kosogova.js
+++ pages/api/lana-kosogova.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const filePath = path.resolve('.', 'assets/lana-kosogova-resume.pdf')
+const filePath = path.resolve('.', 'assets/Resume_Lana Kosogova_Graphic Designer.pdf')
 const imageBuffer = fs.readFileSync(filePath)
 
 export default function fileDownload(req, res) {
