e8555f3cb199a50399db03543208395b1f22d573
PC <denis.kosogov@gmail.com>
2024-10-15 22:32:14 -0400

    update

--- pages/api/lana-kosogova.js
+++ pages/api/lana-kosogova.js
@@ -1,10 +1,13 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
-const filePath = path.resolve('.', 'assets/Resume_Lana Kosogova_Graphic Designer.pdf')
-const imageBuffer = fs.readFileSync(filePath)
+const filePath = path.resolve(
+  ".",
+  "assets/Resume_Lana Kosogova_Graphic Designer.pdf"
+);
+const imageBuffer = fs.readFileSync(filePath);
 
 export default function fileDownload(req, res) {
-  res.setHeader('Content-Type', 'application/pdf')
-  res.send(imageBuffer)
+  res.setHeader("Content-Type", "application/pdf");
+  res.send(imageBuffer);
 }
